54ebe55945d444b0cc215592bce1b496da54b81e
Azharu <azharfrendy@gmail.com>
2023-01-20 21:22:31 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk hide nav
+
+const hamburger = document.querySelector("#hamburger");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});

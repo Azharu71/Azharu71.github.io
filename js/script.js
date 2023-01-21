d4665f87fa3b3a0c141dbbe2627df510590143f4
Azharu <azharfrendy@gmail.com>
2023-01-21 19:45:31 +0700

    update bug

--- js/script.js
+++ js/script.js
@@ -5,13 +5,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger").onclick = () => {
   navbarNav.classList.toggle("active");
 };
-
 // klik diluar sidebar untuk hide nav
 
 const hamburger = document.querySelector("#hamburger");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hamburger.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });

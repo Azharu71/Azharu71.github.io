27a87c8959deb7424f79606214ecd62ff209b72c
Azharu <azharfrendy@gmail.com>
2023-05-24 02:06:30 +0700

    update minor

--- js/script.js
+++ js/script.js
@@ -5,12 +5,23 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+const searchBox = document.querySelector(".search-box");
+document.querySelector("#search").onclick = (e) => {
+  searchBox.classList.toggle("active");
+  e.preventDefault();
+};
 // klik diluar sidebar untuk hide nav
 
 const hamburger = document.querySelector("#hamburger");
+const search = document.querySelector("#search");
+const input = document.querySelector(".search-box input,i");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!search.contains(e.target) && !input.contains(e.target)) {
+    searchBox.classList.remove("active");
   }
 });

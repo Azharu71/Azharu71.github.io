6cf9913cc47382f6c3072b05a7589890c4e32cc5
Azharu <azharfrendy@gmail.com>
2023-08-08 00:16:29 +0700

    menambahkan fitur

--- js/script.js
+++ js/script.js
@@ -17,16 +17,15 @@ const shopCart = document.querySelector(".shoping-cart");
 document.querySelector("#shopping-cart").onclick = (e) => {
   shopCart.classList.toggle("active");
   e.preventDefault();
-}
+};
 
-// klik diluar sidebar untuk hide nav
+// klik diluar element untuk hide nav
 
 const hamburger = document.querySelector("#hamburger");
 const search = document.querySelector("#search");
 const input = document.querySelector(".search-box .form-search");
 const shop = document.querySelector("#shopping-cart");
 const product = document.querySelectorAll(".product");
-
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target)) {
@@ -36,7 +35,31 @@ document.addEventListener("click", function (e) {
     searchBox.classList.remove("active");
   }
 
-  if(!shop.contains(e.target) && !shopCart.contains(e.target)){
-    shopCart.classList.remove('active');
+  if (!shop.contains(e.target) && !shopCart.contains(e.target)) {
+    shopCart.classList.remove("active");
   }
 });
+
+// modal box
+const modal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    modal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klil tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  modal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
